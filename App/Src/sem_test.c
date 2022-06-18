80c4f26348a6b6b17c9a93e13b022da5f024b35d
泡泡纸 <254237702@qq.com>
2022-06-19 01:02:39 +0800

    新功能:添加同步量官方例程1

--- App/Src/sem_test.c
+++ App/Src/sem_test.c
@@ -8,6 +8,101 @@
 
 #if SEM_TEST == 1
 
+#define THREAD_PRIORITY         25
+#define THREAD_TIMESLICE        5
+
+/* 指向信号量的指针 */
+static rt_sem_t dynamic_sem = RT_NULL;
+
+ALIGN(RT_ALIGN_SIZE)
+static char thread1_stack[1024];
+static struct rt_thread thread1;
+static void rt_thread1_entry(void *parameter)
+{
+    static rt_uint8_t count = 0;
+
+    while(1)
+    {
+        if(count <= 100)
+        {
+            count++;
+        }
+        else
+            return;
+
+        /* count 每计数 10 次，就释放一次信号量 */
+         if(0 == (count % 10))
+        {
+            rt_kprintf("t1 release a dynamic semaphore.\n");
+            rt_sem_release(dynamic_sem);
+        }
+    }
+}
+
+ALIGN(RT_ALIGN_SIZE)
+static char thread2_stack[1024];
+static struct rt_thread thread2;
+static void rt_thread2_entry(void *parameter)
+{
+    static rt_err_t result;
+    static rt_uint8_t number = 0;
+    while(1)
+    {
+        /* 永久方式等待信号量，获取到信号量，则执行 number 自加的操作 */
+        result = rt_sem_take(dynamic_sem, RT_WAITING_FOREVER);
+        if (result != RT_EOK)
+        {
+            rt_kprintf("t2 take a dynamic semaphore, failed.\n");
+            rt_sem_delete(dynamic_sem);
+            return;
+        }
+        else
+        {
+            number++;
+            rt_kprintf("t2 take a dynamic semaphore. number = %d\n" ,number);
+        }
+    }
+}
+
+/* 信号量示例的初始化 */
+int semaphore_sample(void)
+{
+    /* 创建一个动态信号量，初始值是 0 */
+    dynamic_sem = rt_sem_create("dsem", 0, RT_IPC_FLAG_PRIO);
+    if (dynamic_sem == RT_NULL)
+    {
+        rt_kprintf("create dynamic semaphore failed.\n");
+        return -1;
+    }
+    else
+    {
+        rt_kprintf("create done. dynamic semaphore value = 0.\n");
+    }
+
+    rt_thread_init(&thread1,
+                   "thread1",
+                   rt_thread1_entry,
+                   RT_NULL,
+                   &thread1_stack[0],
+                   sizeof(thread1_stack),
+                   THREAD_PRIORITY, THREAD_TIMESLICE);
+    rt_thread_startup(&thread1);
+
+    rt_thread_init(&thread2,
+                   "thread2",
+                   rt_thread2_entry,
+                   RT_NULL,
+                   &thread2_stack[0],
+                   sizeof(thread2_stack),
+                   THREAD_PRIORITY-1, THREAD_TIMESLICE);
+    rt_thread_startup(&thread2);
+
+    return 0;
+}
+/* 导出到自动初始化列表 */
+INIT_APP_EXPORT(semaphore_sample);
+
+
 #elif SEM_TEST == 2
 /**
  * @brief 来自官方生产者消费者例程 同步量
@@ -127,7 +222,7 @@ int producer_consumer(void) {
 
     return 0;
 }
-/* 导出到 msh 命令列表中 */
+/* 使用自动化初始化 */
 INIT_APP_EXPORT(producer_consumer);
 
 #endif
