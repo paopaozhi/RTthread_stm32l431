d8f30ee00e13fce036e51c003143ba7bffa04615
泡泡纸 <254237702@qq.com>
2022-06-27 19:49:19 +0800

    重构:优化文件结构

--- RTOS/finsh/cmd.c
+++ RTOS/finsh/cmd.c
@@ -46,7 +46,7 @@ long hello(void)
 
     return 0;
 }
-FINSH_FUNCTION_EXPORT(hello, say hello world);
+MSH_CMD_EXPORT(hello, say hello world);
 
 extern void rt_show_version(void);
 long version(void)
